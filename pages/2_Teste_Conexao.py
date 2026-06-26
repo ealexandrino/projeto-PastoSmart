74e30440088376fffafeb2177a96fa746119d6ed
ealexandrino <personalgrazing@gmail.com>
2026-06-26 16:08:37 -0300

    Update 2_Teste_Conexao.py

--- pages/2_Teste_Conexao.py
+++ pages/2_Teste_Conexao.py
@@ -1,12 +1,14 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
 st.title("Teste de Conexão")
 st.write("Tentando conectar na planilha...")
 
 try:
-    conn = st.connection("gsheets", type="gsheets")
+    conn = st.connection("gsheets", type=GSheetsConnection)
     df = conn.read(worksheet="Dados_limpos")
     st.success("Conectado com sucesso!")
     st.dataframe(df)
+
 except Exception as e:
     st.error(f"Erro ao conectar: {e}")
