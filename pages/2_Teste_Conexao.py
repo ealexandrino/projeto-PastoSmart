f73ef34fafc719d44e505cc5c331b35decac4eba
ealexandrino <personalgrazing@gmail.com>
2026-06-26 16:09:13 -0300

    Update 2_Teste_Conexao.py

--- pages/2_Teste_Conexao.py
+++ pages/2_Teste_Conexao.py
@@ -6,9 +6,14 @@ st.write("Tentando conectar na planilha...")
 
 try:
     conn = st.connection("gsheets", type=GSheetsConnection)
-    df = conn.read(worksheet="Dados_limpos")
-    st.success("Conectado com sucesso!")
+
+    df = conn.read(
+        spreadsheet="1DFy0jTJbv5Mv1n-KtJkTeUuz4uXNjp-khKhPZpQ1m6w",
+        worksheet="Dados_limpos"
+    )
+
+    st.success("✅ Conectado com sucesso!")
     st.dataframe(df)
 
 except Exception as e:
-    st.error(f"Erro ao conectar: {e}")
+    st.error(f"❌ Erro ao conectar: {e}")
